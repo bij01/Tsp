4bfa1b8b6d5baf45f5f836ab07bd71e9310f4830
BIJ <closenight@naver.com>
2022-06-27 18:02:27 +0900

    complete ver 0.1

--- src/com/bank/TransferImpl.java
+++ src/com/bank/TransferImpl.java
@@ -1,60 +1,173 @@
 package com.bank;
 
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.Vector;
 
 public class TransferImpl implements Transfer {
+	Connection con;
+	PreparedStatement pstmt1, pstmt2, pstmt3, pstmt4, pstmt5, pstmt6;
+	public String msg = "";
+	public Vector result = new Vector();
+	
+	void connectDB() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con = DriverManager.getConnection(Info.URL, Info.USR, Info.PWD);
+			con.setAutoCommit(false);
+			System.out.println("오라클 접속성공");
+			pstmt1 = con.prepareStatement(Sql.SQL1);
+			pstmt2 = con.prepareStatement(Sql.SQL2);
+			pstmt3 = con.prepareStatement(Sql.SQL3);
+			pstmt4 = con.prepareStatement(Sql.SQL4);
+			pstmt5 = con.prepareStatement(Sql.SQL5);
+			pstmt6 = con.prepareStatement(Sql.SQL6);
+		} catch (SQLException se) {
+			System.out.println(se);
+		} catch (ClassNotFoundException cnfe) {
+			System.out.println(cnfe);
+		}
+	}
 
 	@Override
 	public boolean isMember(String email) {
-		System.out.println("email");
+		try {
+			pstmt1.setString(1, email);
+			ResultSet rs = pstmt1.executeQuery();
+			if(rs.next()) return true;
+		} catch (SQLException se) {
+		}
+		msg = "수취인 이메일을 확인 할 수 없습니다.";
 		return false;
+		
 	}
 
 	@Override
 	public boolean checkBalance(String email, long amount) {
-		// TODO Auto-generated method stub
+		try {
+			pstmt2.setString(1, email);
+			ResultSet rs = pstmt2.executeQuery();
+			if(rs.next()) {
+				long balance = rs.getLong(1);
+				//System.out.println(email + " 잔액: " + balance);
+				if(balance >= amount) return true;
+			}
+		} catch (SQLException se) {
+		}
+		msg = "잔액이 부족합니다.";
 		return false;
 	}
 
 	@Override
 	public boolean minus(String sender, long amount) {
-		// TODO Auto-generated method stub
+		try{
+			pstmt3.setString(1, sender);
+			pstmt3.setLong(2, amount);
+			pstmt3.setString(3, sender);
+			int i = pstmt3.executeUpdate();
+			if (i>0){
+				return true;
+			}
+		}catch (SQLException se){
+		}
+		msg = "송금에 실패하였습니다.";
 		return false;
 	}
 
 	@Override
 	public boolean plus(String receiver, long amount) {
-		// TODO Auto-generated method stub
+		try{
+			pstmt4.setString(1, receiver);
+			pstmt4.setLong(2, amount);
+			pstmt4.setString(3, receiver);
+			int i = pstmt4.executeUpdate();
+			if (i>0){
+				return true;
+			}
+		}catch (SQLException se){
+		}
+		msg = "수취에 실패하였습니다.";
 		return false;
 	}
 
 	@Override
 	public boolean log(String sender, String receiver, long amount) {
-		// TODO Auto-generated method stub
+		try{
+			pstmt5.setString(1, sender);
+			pstmt5.setString(2, receiver);
+			pstmt5.setLong(3, amount);
+			int i = pstmt5.executeUpdate();
+			if (i>0){
+				//System.out.println("로그 기록 성공");
+				return true;
+			}
+		}catch (SQLException se){
+			System.out.println("로그 예외: " + se);
+		}
 		return false;
 	}
 
 	@Override
 	public void showResult(String sender, String receiver) {
-		// TODO Auto-generated method stub
-
+		try {
+			pstmt6.setString(1, sender);
+			pstmt6.setString(2, receiver);
+			ResultSet rs = pstmt6.executeQuery();
+			while(rs.next()) {
+				String email = rs.getString(1);
+				String name = rs.getString(2);
+				long balance = rs.getLong(3);
+				Date date = rs.getDate(4);
+				Date date2 = rs.getDate(5);
+				result.add(email + ", " + name + ", " + balance + ", " + date + ", " + date2);
+			}
+		} catch (SQLException se) {
+			System.out.println("showResult: " + se);
+		}
 	}
 
 	@Override
 	public void closeALL() {
-		// TODO Auto-generated method stub
-
+		try{
+			pstmt1.close();
+			pstmt2.close();
+			pstmt3.close();
+			pstmt4.close();
+			pstmt5.close();
+			pstmt6.close();
+			con.close();
+		}catch(SQLException se){
+			System.out.println(se);
+		}
 	}
 
 	@Override
 	public boolean transfer(String sender, String receiver, long amount) throws SQLException {
-		// TODO Auto-generated method stub
+		connectDB();
+		if(isMember(receiver)) { //수취인 메일 확인
+			if(checkBalance(sender, amount)) { //송금인 잔액 확인
+				if (minus(sender, amount)) { //송금인 잔액 차감
+					if (plus(receiver, amount)) { //수취인 잔액 증감
+						if (log(sender, receiver, amount)) { //로그 기록
+							//거래가 정상적으로 종료 되었을 경우
+							con.commit();
+							return true;
+						}						
+					}
+				}
+			}
+		}
+		// 거래가 정상적으로 종료되지 않았을 경우 롤백
+		try {
+			con.rollback();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		closeALL();
 		return false;
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//new TransferImpl().init();
 	}
 
 }
--- src/com/user/TsUser.java
+++ src/com/user/TsUser.java
@@ -6,19 +6,22 @@ import com.bank.*;
 public class TsUser {
 
 	public static void main(String[] args) {
-		Transfer t = new TransferImpl();
+		TransferImpl t = new TransferImpl();
 		try {
 			boolean flag = t.transfer("one@daum.net", "two@naver.com", 50000L);
 			if(flag) {
 				System.out.println("이체 성공");
+				t.showResult("one@daum.net", "two@naver.com");
+				System.out.println("보낸 사람: " + t.result.get(0));
+				System.out.println("받는 사람: " + t.result.get(1));
 			} else {
-				System.out.println("이체 실패");
+				System.out.println("이체 실패: " + t.msg);
 			}
 		} catch (SQLException se) {
 			System.out.println("이체 실패(예외): " + se);
 		}
 		
-		t.showResult("one@daum.net", "two@naver.com");
+		
 	}
 
 }
